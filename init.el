152071beba6fe69011c203ce9e7212dd63fd467c
Granville Barnett <granvillebarnett@gmail.com>
2012-05-10 12:10:37 +0100

    Added package management. Seems to be all ticking over fine.

--- init.el
+++ init.el
@@ -1,21 +1,5 @@
 (load "~/.emacs.d/vanilla.el")  
-
-;; package management 
-(require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(package-initialize)
-
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
-;; packages to install if not present
-(defvar my-packages '(autopair paredit magit clojure-mode haskell-mode)
-  "A list of packages to ensure are installed at launch.")
-
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
+(load "~/.emacs.d/package-setup.el")  
 
 ;; key bindings
 (global-set-key (kbd "C-<f10>") 'query-replace)
---
+++ package-setup.el
@@ -0,0 +1,17 @@
+(add-to-list 'load-path "~/.emacs.d/vendor/package")
+
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; packages to install if not present
+(defvar my-packages '(autopair paredit magit clojure-mode haskell-mode)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
