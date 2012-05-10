818f7841b81b7e9c3436f7391e7167df8f4cd272
Granville Barnett <granvillebarnett@gmail.com>
2012-05-10 02:12:25 +0100

    Basic building blocks. Roughly.

---
+++ init.el
@@ -0,0 +1,21 @@
+(load "~/.emacs.d/vanilla.el")  
+
+;; package management 
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; packages to install if not present
+(defvar my-packages '(autopair paredit magit clojure-mode haskell-mode)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+;; key bindings
+(global-set-key (kbd "C-<f10>") 'query-replace)
---
+++ vanilla.el
@@ -0,0 +1,46 @@
+;; UI customisations
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(fset 'yes-or-no-p 'y-or-n-p)            
+(setq inhibit-startup-message t inhibit-startup-echo-area-message t)
+(set-face-attribute 'default nil :height 160)
+(setq ring-bell-function 'ignore)                                   
+(line-number-mode t)                     
+(column-number-mode t)                   
+(size-indication-mode t)                 
+
+;; some basic buffer stuff
+(setq-default fill-column 80)
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
+(setq default-major-mode 'text-mode)
+(global-font-lock-mode t)
+
+
+
+
+(ido-mode t)
+(setq ido-enable-flex-matching t) ; fuzzy matching is a must have
+(setq ido-enable-last-directory-history nil) ; forget latest selected directory
+
+; return a backup file path of a give file path
+; with full directory mirroring from a root dir
+; non-existant dir will be created
+(defun my-backup-file-name (fpath)
+  "Return a new file path of a given file path.
+If the new path's directories does not exist, create them."
+  (let (backup-root bpath)
+    (setq backup-root "~/.emacs.d/emacs-backup")
+    (setq bpath (concat backup-root fpath "~"))
+    (make-directory (file-name-directory bpath) bpath)
+    bpath
+  )
+)
+(setq make-backup-file-name-function 'my-backup-file-name)
+
+(require 'uniquify)
+
+(add-to-list 'default-frame-alist '(width . 85))
+(add-to-list 'default-frame-alist '(height . 100))
+(add-to-list 'default-frame-alist '(left . 250))
+
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
