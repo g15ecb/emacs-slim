1aed6740a64e6e71de3fd14369a7a51bc6a5606a
Granville Barnett <gbarnett@Granvilles-MacBook-Pro.local>
2013-05-14 08:54:35 +0100

    javadoc-lookup.

--- init.el
+++ init.el
@@ -1,19 +1,16 @@
+;; Granville Barnett's Emacs Config
 ;; granvillebarnett@gmail.com
 
 ;; Some of the settings in this file require external tools to have been
 ;; installed, they include:
 
-;; - OCaml and Opam (brew install ocaml opam; opam must be put on PATH)
-;; - opam install merlin ocp-indent
 ;; - LaTeX (MacTeX)
 ;; - AucTeX
+;; - Clang (on OSX this requires installation of XCode and the Command Line Tools)
 
-;; *****************************************************************************
-;; Vanila Settings BEGIN
-;; *****************************************************************************
 
-;; Basic customisations
-(tool-bar-mode -1)
+;; Vanila Settings 
+;; *****************************************************************************
 (scroll-bar-mode -1)
 (fset 'yes-or-no-p 'y-or-n-p)            
 (setq inhibit-startup-message t inhibit-startup-echo-area-message t)
@@ -26,8 +23,6 @@
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
 (setq default-major-mode 'text-mode)
 (global-font-lock-mode t)
-
-;; ido
 (ido-mode t)
 (setq ido-enable-flex-matching t) ; fuzzy matching is a must have
 (setq ido-enable-last-directory-history nil) ; forget latest selected directory
@@ -51,20 +46,18 @@ If the new path's directories does not exist, create them."
 
 (server-start)
 
-(setq-default ispell-program-name "/usr/local/bin/aspell")
+;; (setq-default ispell-program-name "/usr/local/bin/aspell")
+;; (setq ispell-dictionary "uk")
 (setq mac-command-modifier 'meta)
 
-;; *****************************************************************************
-;; Vanila Settings END
-;; *****************************************************************************
 
-;; ~~~~~
 
-;; *****************************************************************************
-;; Elpa BEGIN
+
+;; Elpa 
 ;; *****************************************************************************
 
 (require 'package)
+
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                            ("marmalade" . "http://marmalade-repo.org/packages/")
@@ -81,71 +74,56 @@ If the new path's directories does not exist, create them."
 		      browse-kill-ring
 		      perspective
 		      rainbow-delimiters
-		      tuareg
+		      ;; tuareg
 		      auto-complete-clang
-		      haskell-mode
-		      evil)
-		      
-  "A list of packages to ensure are installed at launch.")
+		      ;; haskell-mode
+		      rust-mode
+		      javadoc-lookup
+		      d-mode
+		      evil))
+
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
 
-;; *****************************************************************************
-;; Elpa END
+
+
+
+;; Package Setup 
 ;; *****************************************************************************
 
-;; ~~~~
+(evil-mode)
 
-;; *****************************************************************************
-;; Package Setup BEGIN
-;; *****************************************************************************
-
-(evil-mode) ;; beautiful
 
 (persp-mode)
-(browse-kill-ring-default-keybindings) 	; M-y to browse kill ring
+
+
+(browse-kill-ring-default-keybindings) 	; m-y to browse kill ring
+
 
 (require 'autopair)
 
-;; AC BEGIN -----------------------------------------------------
+
+;; javadoc-lookup
+;; http://nullprogram.com/blog/2013/01/30/
+(require 'javadoc-lookup)
+(javadoc-add-roots "~/java-docs/api")
+
+
+;; autocomplete 
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 (ac-config-default)
-;; make auto-complete show immediately
-(setq ac-auto-show-menu 0.)
-;; AC END -------------------------------------------------------
-
-;; OCaml BEGIN --------------------------------------------------
-;; NB. Prerequisites:
-;; - brew install opam
-;; - opam install utop (https://github.com/diml/utop)
-;; - opam install merlin (https://github.com/def-lkb/merlin)
-;; - opam install ocp-indent (https://github.com/OCamlPro/ocp-indent)
-;; The load paths are what opam will dump stuff in
-(add-to-list 'load-path "~/.opam/4.00.1/share/emacs/site-lisp/")
-(require 'merlin)
-
-(add-hook 'tuareg-mode-hook 'merlin-mode)
-
-;; tuareg will use ocp-indent as the default indent engine if you have the following
-(load-file "~/.opam/4.00.1/share/typerex/ocp-indent/ocp-indent.el")
-
-;; cd ~/.emacs then do a git clone git@github.com:diml/utop.git
-(add-to-list 'load-path "~/.emacs.d/utop/src/top")
-(autoload 'utop-setup-ocaml-buffer "utop" "Toplevel for OCaml" t)
-(add-hook 'tuareg-mode-hook 'utop-setup-ocaml-buffer)
-;; OCaml END ----------------------------------------------------
-
-;; auto-complete-clang BEGIN ------------------------------------
+(setq ac-auto-show-menu 0.)		; show immediately
+;; auto-complete-clang 
 (require 'auto-complete-clang)
 (defun my-ac-cc-mode-setup ()
 (setq ac-sources (append '(ac-source-clang ac-source-yasnippet) ac-sources)))
 (add-hook 'c-mode-common-hook 'my-ac-cc-mode-setup)
-;; auto-complete-clang END --------------------------------------
 
-;; AucTeX BEGIN -------------------------------------------------
+
+;; AucTeX 
 ;; rake; then cd into auctex dir
 ;; ./configure --with-texmf-dir=/usr/local/texlive/texmf-local
 ;; make
@@ -166,46 +144,38 @@ If the new path's directories does not exist, create them."
  (setq reftex-cite-format 'natbib)
 
  (add-hook 'LaTeX-mode-hook 'reftex-mode)
-;; AucTeX END ---------------------------------------------------
 
+
+
+
+;; Hooks 
 ;; *****************************************************************************
-;; Package Setup END
-;; *****************************************************************************
+(defun java-hooks() 
+  (autopair-mode)
+  (show-paren-mode)
+  (rainbow-delimiters-mode)
+  (local-set-key (kbd "M-p") 'javadoc-lookup))
 
+(add-hook 'java-mode-hook 'java-hooks)
 
-;; *****************************************************************************
-;; Hooks BEGIN
-;; *****************************************************************************
-;; (defun default-hooks() 
-;;   (autopair-mode)
-;;   (show-paren-mode)
-;;   (rainbow-delimiters-mode))
+;; (defun ocaml-hooks()
+;;   (local-set-key (kbd "M-p") 'tuareg-eval-phrase)
+;;   (local-set-key (kbd "M-i") 'merlin-try-completion))
 
-;; (add-hook tuareg-mode-hook 'default-hooks)
+;; (add-hook 'tuareg-mode-hook 'ocaml-hooks)
 
-(defun ocaml-hooks()
-  (local-set-key (kbd "M-p") 'tuareg-eval-phrase))
+;; (defun haskell-hooks()
+;;   (local-set-key (kbd "M-p") 'inferior-haskell-load-file)
+;;   (haskell-doc-mode))
 
-(add-hook 'tuareg-mode-hook 'ocaml-hooks)
-
-(defun haskell-hooks()
-  (local-set-key (kbd "M-p") 'inferior-haskell-load-file)
-  (haskell-doc-mode))
-
-(add-hook 'haskell-mode-hook 'haskell-hooks)
+;; (add-hook 'haskell-mode-hook 'haskell-hooks)
 
 
 
-;; *****************************************************************************
-;; Hooks END
-;; *****************************************************************************
 
-
-
-;; Keybindings BEGIN
+;; Global Keybindings 
 ;; *****************************************************************************
 (global-set-key (kbd "M-3") '(lambda () (interactive) (insert "#")))
-
 
 ;; replace C-x f and C-x b with some nicer alternatives
 (global-set-key (kbd "M-f") 'ido-find-file)
@@ -230,7 +200,9 @@ If the new path's directories does not exist, create them."
 ;; (global-set-key [f9] 'ns-toggle-fullscreen)
 
 
-;; Keybindings END
+
+
+;; VARIABLES
 ;; *****************************************************************************
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -238,7 +210,8 @@ If the new path's directories does not exist, create them."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ac-auto-start 1)
- '(ac-modes (quote (emacs-lisp-mode lisp-mode lisp-interaction-mode slime-repl-mode c-mode cc-mode c++-mode go-mode java-mode malabar-mode clojure-mode clojurescript-mode scala-mode scheme-mode ocaml-mode tuareg-mode coq-mode haskell-mode agda-mode agda2-mode perl-mode cperl-mode python-mode ruby-mode lua-mode ecmascript-mode javascript-mode js-mode js2-mode php-mode css-mode makefile-mode sh-mode fortran-mode f90-mode ada-mode xml-mode sgml-mode ts-mode sclang-mode verilog-mode LaTeX-mode latex-mode))))
+ '(ac-modes (quote (emacs-lisp-mode d-mode lisp-mode lisp-interaction-mode slime-repl-mode c-mode cc-mode c++-mode go-mode java-mode malabar-mode clojure-mode clojurescript-mode scala-mode scheme-mode ocaml-mode tuareg-mode coq-mode haskell-mode agda-mode agda2-mode perl-mode cperl-mode python-mode ruby-mode lua-mode ecmascript-mode javascript-mode js-mode js2-mode php-mode css-mode makefile-mode sh-mode fortran-mode f90-mode ada-mode xml-mode sgml-mode ts-mode sclang-mode verilog-mode LaTeX-mode latex-mode rust-mode)))
+ '(python-shell-interpreter "python3.3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
