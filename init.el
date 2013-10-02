54831f00d0ac9022afc077543e720b861e8d4772
Granville Barnett <granville.barnett@hp.com>
2013-10-02 10:12:19 +0100

    AC for bibtex

--- init.el
+++ init.el
@@ -124,11 +124,6 @@ If the new path's directories does not exist, create them."
 
 (add-hook 'LaTeX-mode-hook 'reftex-mode)
 
-(defun latex-hooks()
-  (flyspell-mode))
-
-(add-hook 'LaTeX-mode-hook 'latex-hooks)
-
 ;; Hooks 
 ;; *****************************************************************************
 (defun common-hooks() 
@@ -196,7 +191,7 @@ If the new path's directories does not exist, create them."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ac-modes (quote (emacs-lisp-mode lisp-mode latex-mode LaTeX-mode lisp-interaction-mode slime-repl-mode c-mode cc-mode c++-mode go-mode java-mode malabar-mode clojure-mode clojurescript-mode scala-mode scheme-mode ocaml-mode tuareg-mode coq-mode haskell-mode agda-mode agda2-mode perl-mode cperl-mode python-mode ruby-mode lua-mode ecmascript-mode javascript-mode js-mode js2-mode php-mode css-mode makefile-mode sh-mode fortran-mode f90-mode ada-mode xml-mode sgml-mode ts-mode sclang-mode verilog-mode markdown-mode)))
+ '(ac-modes (quote (emacs-lisp-mode bibtex-mode lisp-mode latex-mode LaTeX-mode lisp-interaction-mode slime-repl-mode c-mode cc-mode c++-mode go-mode java-mode malabar-mode clojure-mode clojurescript-mode scala-mode scheme-mode ocaml-mode tuareg-mode coq-mode haskell-mode agda-mode agda2-mode perl-mode cperl-mode python-mode ruby-mode lua-mode ecmascript-mode javascript-mode js-mode js2-mode php-mode css-mode makefile-mode sh-mode fortran-mode f90-mode ada-mode xml-mode sgml-mode ts-mode sclang-mode verilog-mode markdown-mode)))
  '(custom-safe-themes (quote ("b1e54397de2c207e550dc3a090844c4b52d1a2c4a48a17163cce577b09c28236" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
