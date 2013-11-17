79d9dfca71e507ca54008d8746ffb046698eb59b
Granville Barnett <granvillebarnett@gmail.com>
2013-11-17 17:49:14 +0000

    ipython

--- init.el
+++ init.el
@@ -84,6 +84,7 @@ If the new path's directories does not exist, create them."
 		      erlang
 		      scala-mode2
 		      tuareg
+		      fsharp-mode
 		      evil)
   "A list of packages to ensure are installed at launch.")
 
@@ -266,3 +267,17 @@ If the new path's directories does not exist, create them."
 (setq mac-option-modifier 'super)
 (setq mac-command-modifier 'meta)
 (set-face-attribute 'default nil :height 200)
+(global-unset-key (kbd "M-3"))
+(global-set-key (kbd "M-3") '(lambda() (interactive) (insert-string "#")))
+
+(setq
+ python-shell-interpreter "ipython3"
+ python-shell-interpreter-args ""
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+ python-shell-completion-setup-code
+   "from IPython.core.completerlib import module_completion"
+ python-shell-completion-module-string-code
+   "';'.join(module_completion('''%s'''))\n"
+ python-shell-completion-string-code
+   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
