7c9f43b5e443a68041656bd1d01602164012deb0
Granville Barnett <granvillebarnett@gmail.com>
2013-12-30 21:53:55 +0000

    Sml.

--- init.el
+++ init.el
@@ -73,6 +73,7 @@ If the new path's directories does not exist, create them."
                       auto-complete
 		      solarized-theme
 		      tuareg
+		      sml-mode
 		      evil)
   "A list of packages to ensure are installed at launch.")
 
@@ -124,6 +125,10 @@ If the new path's directories does not exist, create them."
 (setq merlin-use-auto-complete-mode t)
 (autoload 'utop-setup-ocaml-buffer "utop" "Toplevel for OCaml" t)
 (load-file "~/.opam/4.01.0/share/emacs/site-lisp/ocp-indent.el")
+
+;; f#
+(setq inferior-fsharp-program "/usr/bin/fsharpi --readline-")
+(setq fsharp-compiler "usr/bin/fsharpc")
 
 ;; *****************************************************************************
 ;; Hooks 
@@ -215,17 +220,13 @@ If the new path's directories does not exist, create them."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ac-modes (quote (emacs-lisp-mode prolog-mode
- prolog-inferior-mode bibtex-mode d-mode lisp-mode latex-mode
- LaTeX-mode lisp-interaction-mode slime-repl-mode c-mode cc-mode
- c++-mode go-mode java-mode malabar-mode clojure-mode
- clojurescript-mode scala-mode scheme-mode ocaml-mode tuareg-mode
- coq-mode haskell-mode agda-mode agda2-mode perl-mode erlang-mode
- cperl-mode python-mode ruby-mode lua-mode ecmascript-mode
- javascript-mode js-mode js2-mode php-mode css-mode makefile-mode
- sh-mode fortran-mode f90-mode ada-mode xml-mode sgml-mode
- ts-mode verilog-mode markdown-mode sml-mode erlang-shell-mode
- rust-mode inferior-sml-mode)))
- '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "b1e54397de2c207e550dc3a090844c4b52d1a2c4a48a17163cce577b09c28236"
- default))))
+ '(ac-modes (quote (emacs-lisp-mode prolog-mode prolog-inferior-mode bibtex-mode d-mode lisp-mode latex-mode fsharp-mode inferior-fsharp-mode LaTeX-mode lisp-interaction-mode slime-repl-mode c-mode cc-mode c++-mode go-mode java-mode malabar-mode clojure-mode clojurescript-mode scala-mode scheme-mode ocaml-mode tuareg-mode coq-mode haskell-mode agda-mode agda2-mode perl-mode erlang-mode cperl-mode python-mode ruby-mode lua-mode ecmascript-mode javascript-mode js-mode js2-mode php-mode css-mode makefile-mode sh-mode fortran-mode f90-mode ada-mode xml-mode sgml-mode ts-mode verilog-mode markdown-mode sml-mode erlang-shell-mode rust-mode inferior-sml-mode)))
+ '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "b1e54397de2c207e550dc3a090844c4b52d1a2c4a48a17163cce577b09c28236" default)))
+ '(sml-indent-level 2))
 ;; -----------------------------------------------------------------------------
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
